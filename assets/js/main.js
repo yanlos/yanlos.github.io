213af766047c4bac5ac97fead31e68eac7cb4e21
yanlos <yanlos@yahoo.com>
2020-05-12 04:23:10 -0700

    information update
    added favicon
    new banner
    added FAQs
    added Calendar that can be synced with google calendars to show our classes each week / spots open

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,34 +42,34 @@
 
 	// Nav.
 
-		// Title Bar.
-//			$(
-//				'<div id="titleBar">' +
-//					'<a href="#navPanel" class="toggle"></a>' +
-//					'<span class="title">' + $('#logo').html() + '</span>' +
-//				'</div>'
-//			)
-//				.appendTo($body);
+//		 Title Bar
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
 
 		// Panel.
-//			$(
-//				'<div id="navPanel">' +
-//					'<nav>' +
-//						$('#nav').navList() +
-//					'</nav>' +
-//				'</div>'
-//			)
-//				.appendTo($body)
-//				.panel({
-//					delay: 500,
-//					hideOnClick: true,
-//					hideOnSwipe: true,
-//					resetScroll: true,
-//					resetForms: true,
-//					side: 'left',
-//					target: $body,
-//					visibleClass: 'navPanel-visible'
-//				});
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
 	// Parallax.
 	// Disabled on IE (choppy scrolling) and mobile platforms (poor performance).
@@ -246,5 +246,28 @@
 
 		$banner
 			._parallax();
+    
+    
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+    
+
+
 
 })(jQuery);
